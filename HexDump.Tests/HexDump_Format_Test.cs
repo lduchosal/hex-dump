f7e63d1923394b68d02b01e8b906969c5a22dddb
q <luc+github@2113.ch>
2020-03-29 23:13:11 +0200

    Added Parse functionnality and some unit tests

---
+++ HexDump.Tests/HexDump_Format_Test.cs
@@ -0,0 +1,193 @@
+using System.Linq;
+using Xunit;
+
+namespace HexDump.Tests
+{
+    public class HexDump_Format_Test
+    {
+
+        [Fact]
+        public void When_Encode_16_Then_ok()
+        {
+            var data = new byte[16];
+            var expected = @"
+0000    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+".Trim();
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void When_Encode_32_Then_ok()
+        {
+            var data = new byte[32];
+            var expected = @"
+0000    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0010    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+".Trim();;
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public void When_Encode_48_Then_ok()
+        {
+            var data = new byte[48];
+            var expected = @"
+0000    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0010    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0020    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+".Trim();
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public void When_Encode_256_Then_ok()
+        {
+            var data = new byte[256];
+            var expected = @"
+0000    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0010    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0020    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0030    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0040    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0050    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0060    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0070    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0080    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0090    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+00A0    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+00B0    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+00C0    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+00D0    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+00E0    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+00F0    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+".Trim();
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public void When_Encode_8_Then_ok()
+        {
+            var data = new byte[8];
+            var expected = @"
+0000    00 00 00 00 00 00 00 00                             ........         ".TrimStart();
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void When_Encode_0_Then_ok()
+        {
+            var data = new byte[0];
+            var expected = @"";
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public void When_Encode_24_Then_ok()
+        {
+            var data = new byte[24];
+            var expected = @"
+0000    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0010    00 00 00 00 00 00 00 00                             ........         ".TrimStart();
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void When_Encode_40_Then_ok()
+        {
+            var data = new byte[40];
+            var expected = @"
+0000    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0010    00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00    ........ ........
+0020    00 00 00 00 00 00 00 00                             ........         ".TrimStart();
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void When_Encode_40_one_Then_ok()
+        {
+            var data = Enumerable.Repeat((byte) 1, 40).ToArray();
+
+            var expected = @"
+0000    01 01 01 01 01 01 01 01  01 01 01 01 01 01 01 01    ........ ........
+0010    01 01 01 01 01 01 01 01  01 01 01 01 01 01 01 01    ........ ........
+0020    01 01 01 01 01 01 01 01                             ........         ".TrimStart();
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void When_Encode_40_61_Then_ok()
+        {
+            var data = Enumerable.Repeat((byte) 'a', 40).ToArray();
+
+            var expected = @"
+0000    61 61 61 61 61 61 61 61  61 61 61 61 61 61 61 61    aaaaaaaa aaaaaaaa
+0010    61 61 61 61 61 61 61 61  61 61 61 61 61 61 61 61    aaaaaaaa aaaaaaaa
+0020    61 61 61 61 61 61 61 61                             aaaaaaaa         ".TrimStart();
+            
+            var result = HexDump.Format(data);
+            var parsed = HexDump.Parse(result);
+            var result2 = HexDump.Format(parsed);
+
+            Assert.Equal(expected, result2);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ HexDump/HexDump_Parse.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Drew Noakes. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HexDump
+{
+    public  static partial class HexDump
+    {
+        
+        private static readonly Regex _re =
+            new Regex(@"^((?<offset>[0-9a-f]+)\s+)?(?<hexa>[0-9a-f\s]{48})\s+(?<dump>.+)?$",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        /// <summary>
+        /// Parse HexDump into a byte array
+        /// Support only hexdumped bytes array with these attributes:
+        /// - columnWidth = 8
+        /// - columnCount = 2
+        /// - includeOffset = true
+        /// - includeAscii = true
+        ///
+        /// </summary>
+        /// <param name="dump"></param>
+        /// <returns></returns>
+        public static byte[] Parse(string dump )
+        {
+            //00000000   01 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00   ................
+            //00000000   01 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00   ................
+
+            var result = new List<byte>();
+            var lines = dump.Split(Environment.NewLine.ToCharArray());
+            foreach (var line in lines)
+            {
+                var capture = _re.Match(line);
+                if (!capture.Success) continue;
+                var hexa = capture.Groups["hexa"]
+                        .Value
+                        .Replace(" ", "")
+                    ;
+
+                var bytes = Enumerable.Range(0, hexa.Length)
+                    .Where(x => x % 2 == 0)
+                    .Select(x => Convert.ToByte(hexa.Substring(x, 2), 16))
+                    .ToArray();
+
+                result.AddRange(bytes);
+            }
+
+            return result.ToArray();
+        }
+
+    }
+}
